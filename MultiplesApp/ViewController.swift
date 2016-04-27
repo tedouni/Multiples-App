20e74792b9dc013a6a47fea053d02ef5b04e98d0
Tayseer <taedouni@gmail.com>
2016-04-27 00:18:57 -0700

    Completed app according to Mark Price's requirements for Multiples App

--- MultiplesApp/ViewController.swift
+++ MultiplesApp/ViewController.swift
@@ -10,8 +10,87 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Initialize values
+    var sum = 0
+    var multiple = 0
+    let counterMultiple = 5
+    var counter = 0
+    
+    @IBOutlet weak var titleImage: UIImageView!
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBOutlet weak var multipleTextFieldValue: UITextField!
+    
+    
+    @IBAction func multipleTextField(sender: AnyObject) {
+        
+        
+        guard var _ = Int(multipleTextFieldValue.text!)else{
+            //doesn't pass
+            multiple = 0
+            
+            return
+        }
+        //passes
+        multiple = Int(multipleTextFieldValue.text!)!
+   
+
+    }
+    
+    
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBAction func playButtonPressed(sender: AnyObject) {
+   
+        multiple = Int(multipleTextFieldValue.text!)!
+        titleImage.hidden = true
+        playButton.hidden = true
+        multipleTextFieldValue.hidden = true
+        
+        outputLabel.text = "\(sum) + \(multiple) = \(sum+multiple)"
+        sum = sum + multiple
+        counter += 1
+        outputLabel.hidden = false
+        addButton.hidden = false
+        
+
+    }
+    
+    
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBAction func addButtonPressed(sender: AnyObject) {
+        
+        if (counter > counterMultiple) {
+            sum = 0
+            counter = 0
+            outputLabel.hidden = true
+            addButton.hidden = true
+            titleImage.hidden = false
+            playButton.hidden = false
+            multipleTextFieldValue.hidden = false
+            
+        }else{
+            outputLabel.text = "\(sum) + \(multiple) = \(sum+multiple)"
+            sum = sum + multiple
+            print(sum)
+            counter += 1
+        }
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        outputLabel.hidden = true
+        addButton.hidden = true
+        
+        
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +100,9 @@ class ViewController: UIViewController {
     }
 
 
+    
+    func dismissKeyboard(){
+        view.endEditing(true)
+    }
 }
 
